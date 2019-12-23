10aa6ef212ce80feceeb21549569515b6cfb9e39
mysteriousVP <guniv12@gmail.com>
2019-12-23 16:35:42 +0200

    first commit

---
+++ NetworkAPI/DAL/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetworkAPI/NetworkAPI/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetworkAPI
+{
+    public class Class1
+    {
+    }
+}
